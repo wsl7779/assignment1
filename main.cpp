778b64425fa12e22fc16e35dcf7d3c11236166bb
Will <wsl7779@gmail.com>
2023-09-30 23:33:22 -0400

    added header

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/**
+* Author: William Lee
+* Assignment: Simple 2D Scene
+* Date due: 2023-09-30, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
